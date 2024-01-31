a05d11524e45e5a50acfa806b00685ef63f9f431
Mcdaid101 <matthewmcdaid1@hotmail.com>
2024-01-31 14:38:15 +0000

    installed packages and set up axios

--- pick_a_beer/index.js
+++ pick_a_beer/index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import axios from "axios";
 
 const app = express();
 const port = 3000; 
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello world</h1>");
-});
+
+app.get("/", async (req, res) => {
+    try {
+      const result = await axios.get('https://api.punkapi.com/v2/beers/random');
+      console.log(result);
+      res.render("index.ejs", {
+        
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500);
+    }
+  });
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
