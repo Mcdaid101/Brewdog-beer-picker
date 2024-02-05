a3b57b4b4f05a8e924f60548a2fa45278a0ad918
Mcdaid101 <matthewmcdaid1@hotmail.com>
2024-02-05 21:16:22 +0000

    beers from year selected now render on their own page

--- pick_a_beer/index.js
+++ pick_a_beer/index.js
@@ -10,34 +10,33 @@ app.use(express.static("public"));
 
 
 app.get("/", async (req, res) => {
-    try {
-      const result = await axios.get('https://api.punkapi.com/v2/beers/random');
-      console.log(result);
-      res.render("index.ejs", {
-        name: result.data[0]
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500);
-    }
-  });
+  try {
+    const result = await axios.get('https://api.punkapi.com/v2/beers/random');
+    console.log(result);
+    res.render("index.ejs", { beers: [result.data[0]] });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-// displays what brew dogs were brewed in a certain year chosen by the user 
- app.post("/", async (req, res) => {
-    try {
-      console.log(req.body);
-      const year = req.body.year;
-      const response = await axios.get(
-        `https://api.punkapi.com/v2/beers?brewed_before=12-${year}&brewed_after=01-${year}`
-      );
-      const result = response.data;
-      console.log(result);
-      res.render("index.ejs", {beers: result});
-    } catch (error) {
-      console.error("Failed to make request:", error.message);
-    }
-  });
+
+app.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const year = req.body.year;
+    const response = await axios.get(
+      `https://api.punkapi.com/v2/beers?brewed_before=12-${year}&brewed_after=01-${year}`
+    );
+    const beerYears = response.data;
+    console.log(beerYears);
+    res.render("beerYears.ejs", { beers: beerYears });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-  });
+  console.log(`Server running on port: ${port}`);
+});
